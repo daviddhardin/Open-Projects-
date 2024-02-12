0ef14427e7bbed09bdffe2dbc6e810cd9df2947f
daviddhardin <141267353+daviddhardin@users.noreply.github.com>
2024-02-12 18:38:32 -0500

    Add files via upload

---
+++ create.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let button = document.querySelector("#create");
+    button.addEventListener('click', function() {
+        button.style.backgroundColor = 'red';
+    });
+});
